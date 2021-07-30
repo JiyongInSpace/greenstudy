507fb9be176d696c4ab971178ca5e6b2fef21494
JiyongInSpace <blaziken@naver.com>
2021-07-31 00:04:07 +0900

    0802test

---
+++ KJY/0802/main.js
@@ -0,0 +1,42 @@
+const logo = document.querySelector(".logo");
+const imgs = document.querySelectorAll(".buttons img");
+const btns = document.querySelectorAll(".colorBtn")
+const items = document.querySelector(".items");
+
+function show(item, index){
+    items.innerHTML +=
+    `<li class="item">
+        <img src="${item.image}" alt="item${index}" class="item__thumbnail">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>`
+}
+
+function showItems(){
+    fetch("data/data.json")
+    .then(res => res.json())
+    .then(data => callback(data, this.dataset.value, this.className));
+    items.innerHTML = '';
+    function callback(data, value, cn){
+        if(cn === "logo"){
+            data.items.forEach((item, index) => {
+                show(item, index);
+            })
+        } else {
+            data.items.forEach((item, index) => {
+                if(item.type === value){
+                    show(item, index);
+                } else if (item.color === value) {
+                    show(item, index);
+                }
+            })
+        }
+    }
+}
+
+logo.addEventListener("click", showItems);
+btns.forEach((btn) =>{
+    btn.addEventListener("click", showItems);
+})
+imgs.forEach((img) => {
+    img.addEventListener("click", showItems);
+})
