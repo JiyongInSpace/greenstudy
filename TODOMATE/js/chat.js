22f58826eaad6878e9cd344a8057057770dbc717
unknown <pool1129@naver.com>
2021-10-04 20:59:04 +0900

    2021.10.04

--- TODOMATE/js/chat.js
+++ TODOMATE/js/chat.js
@@ -10,7 +10,6 @@ const wrap = document.querySelector('.wrapper'),
       closeBtn = document.querySelector('.close'),
       downBtn = document.querySelector('.downClose');
 
-    
     chat_thum.addEventListener('click',function(){
         wrap.classList.add('show');
         chat_thum.style.display="none";
@@ -48,8 +47,71 @@ function send(){
         name : nickname.value,
         msg : chatInput.value
     }
-
     socket.emit("chatting",param);
+}
+
+function todoDate(){
+    const v = chatInput.value;
+    var month = v.split('월');
+    var day = month[1].split('일');
+
+    month = month[0]
+    day = day[0].trim();
+
+    if(month<10){
+        month = `0${month}`;
+    }
+    if(day<10){
+        day = `0${day}`;
+    }
+
+    var date = `${new Date().getFullYear()}${month}${day}`;
+    const local = JSON.parse(localStorage.getItem('todos'));
+    const li = document.createElement('li');
+    var receivedDom;
+
+    const array = [];
+
+    //일정이 여러개면 여러개 나오게 해야함
+
+    for(let i=0; i<local.length; i++){
+        if(local[i].day==date){
+            array.push(local[i].text);
+        }
+    }
+
+    console.log(array)
+
+    if(array.length == 0){
+        receivedDom =
+                        `<span class="profile">
+                            <img src="img/chat_img/1.png" alt="any" class="image">
+                        </span>
+                        <span class="text">
+                            <span class="user">Todomate</span>
+                            <div class="msg1">
+                                <span class="message">해당 날짜에 일정이 없습니다.</span>
+                                <span class="time"></span> 
+                            </div>
+                        </span>`
+    }else if(array != null){
+        receivedDom =
+                        `<span class="profile">
+                            <img src="img/chat_img/1.png" alt="any" class="image">
+                        </span>
+                        <span class="text">
+                            <span class="user">Todomate</span>
+                            <div class="msg">
+                            <span class="message">${array}이 있습니다.</span>
+                                <span class="time"></span> 
+                            </div>
+                        </span>`
+    }
+
+    li.classList.add('received');
+    li.innerHTML = receivedDom;
+    chatList.appendChild(li);
+
     chatInput.value = "";
 }
 
@@ -57,13 +119,18 @@ sendButton.addEventListener('click',()=>{
     send();
 })
 
-
 socket.on("chatting",(data)=>{
     const {name, msg, time} = data;
     const item = new LiModel(name, msg, time);
 
     item.makeLi();
     displayContainer.scrollTo(0, displayContainer.scrollHeight);
+
+    if(!isNaN(chatInput.value.charAt(0))){
+        todoDate()
+    }else{
+        chatInput.value = "";
+    }
 })
 
 function LiModel(name,msg,time){
