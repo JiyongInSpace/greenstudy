dbf8a6e30729701c019b50c2b44928a1db038df6
unknown <pool1129@naver.com>
2021-10-11 13:39:54 +0900

    2021.10.11

--- TODOMATE/js/chat.js
+++ TODOMATE/js/chat.js
@@ -10,6 +10,8 @@ const wrap = document.querySelector('.wrapper'),
       closeBtn = document.querySelector('.close'),
       downBtn = document.querySelector('.downClose');
 
+
+    //관리자 처음 인사말
     chat_thum.addEventListener('click',function(){
         wrap.classList.add('show');
         chat_thum.style.display="none";
@@ -20,18 +22,26 @@ const wrap = document.querySelector('.wrapper'),
                                     <span class="text">
                                         <span class="user">Todomate</span>
                                         <div class="msg">
-                                            <span class="message">안녕하세요! 반가워요</span>
+                                            <span class="message">
+                                                안녕하세요! 반가워요 <br/>
+
+                                                비서봇이 도와줘요! <br/><br/>
+                                                    10월 2일 (해당일정) 넣어줘
+                                                    10월 2일 일정 알려줘
+                                            </span>
                                             <span class="time"></span> 
                                         </div>
                                     </span>
                                 </li>`
     });
 
+    //닫기 버튼
     closeBtn.addEventListener('click',function(){
         wrap.classList.remove('show');
         chat_thum.style.display="block";
     });
 
+    //아래로 내리기 버튼
     downBtn.addEventListener('click',function(){
         if(wrap.classList.contains('down')){
             wrap.classList.remove('down');
@@ -50,8 +60,13 @@ function send(){
     socket.emit("chatting",param);
 }
 
-function todoDate(){
-    const v = chatInput.value;
+const li = document.createElement('li');
+var receivedDom;
+
+//'일정 알려줘' 함수
+function infoTodo(){
+    var v = chatInput.value;
+    //날짜 형식을 ex)(올해 년도)1001 로 변경
     var month = v.split('월');
     var day = month[1].split('일');
 
@@ -67,8 +82,6 @@ function todoDate(){
 
     var date = `${new Date().getFullYear()}${month}${day}`;
     const local = JSON.parse(localStorage.getItem('todos'));
-    const li = document.createElement('li');
-    var receivedDom;
 
     const array = [];
 
@@ -115,6 +128,69 @@ function todoDate(){
     chatInput.value = "";
 }
 
+//'일정 넣어줘'함수
+function putTodo(){
+    var v = chatInput.value;
+    //날짜 형식을 ex)(올해 년도)1001 로 변경
+    var month = v.split('월');
+    var day = month[1].split('일');
+
+    month = month[0]
+    day = day[0].trim(); //공백 제거
+
+    //일정 text 문자 추출
+    var text = v.replace('넣어줘','');
+        text = text.replace(`${month}월`,'');
+        text = text.replace(`${day}일`,'');
+
+        text = text.trim();
+
+    if(month<10){
+        month = `0${month}`;
+    }
+    if(day<10){
+        day = `0${day}`;
+    }
+
+    var date = `${new Date().getFullYear()}${month}${day}`;
+
+    const todoObj = {
+        day: date,
+        goal: 1,
+        text: text,
+        id: Date.now(),
+        checked: false
+    };
+    
+    const exist = JSON.parse(localStorage.getItem('todos'));
+    if(exist == null) exist = [];
+
+
+
+    localStorage.setItem('todos',JSON.stringify(todoObj));
+    exist.push(todoObj);
+    localStorage.setItem("todos",JSON.stringify(exist));
+
+    receivedDom =
+                `<span class="profile">
+                    <img src="img/chat_img/1.png" alt="any" class="image">
+                </span>
+                <span class="text">
+                    <span class="user">Todomate</span>
+                    <div class="msg1">
+                        <span class="message">해당 날짜에 일정을 넣었습니다.</span>
+                        <span class="time"></span> 
+                    </div>
+                </span>`
+
+    li.classList.add('received');
+    li.innerHTML = receivedDom;
+    chatList.appendChild(li);
+
+    chatInput.value = "";
+
+}
+
 sendButton.addEventListener('click',()=>{
     send();
 })
@@ -127,7 +203,17 @@ socket.on("chatting",(data)=>{
     displayContainer.scrollTo(0, displayContainer.scrollHeight);
 
     if(!isNaN(chatInput.value.charAt(0))){
-        todoDate()
+
+        var message = document.querySelectorAll('.message');
+
+            message = message[message.length-1].innerHTML;
+
+        if(message.slice(-3) == '알려줘'){
+            infoTodo();
+        }else if(message.slice(-3) == '넣어줘'){
+            putTodo();
+            //일정 넣고 새로고침해야 들어감---?
+        }
     }else{
         chatInput.value = "";
     }
